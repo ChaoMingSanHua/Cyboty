6b87bfeea0d3174e1e175a77f1091a27a517b3be
Cybaster <1219925930@qq.com>
2023-04-29 17:55:28 +0800

    add sdh and mdh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,97 @@
 <template>
   <v-card class="model-container w-50">
-    <h1 class="text-center">结构配置</h1>
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-center v-col-6">
-            Name
-          </th>
-          <th class="text-center v-col-12">
-            Value
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) of paras">
-          <td class="text-center">{{item.name}}</td>
-          <td class="text-center">
-            <input type="number" class="text-center w-75" v-model="paras[index].value"/>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
+    <v-row class="my-4">
+      <v-col>
+        <div class="text-center text-h4 font-weight-bold">结构配置</div>
+        <v-table>
+          <thead>
+          <tr>
+            <th class="text-center v-col-6">
+              Name
+            </th>
+            <th class="text-center v-col-6">
+              Value
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item, index) of paras">
+            <td class="text-center">{{item.name}}</td>
+            <td class="text-center">
+              <input type="number" class="text-center w-75" v-model="paras[index].value"/>
+            </td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+    <v-row class="my-4">
+      <v-col>
+        <div class="text-center text-h4 font-weight-bold">关节限位</div>
+        <v-table>
+          <thead>
+          <tr>
+            <th class="text-center v-col-6">
+              Name
+            </th>
+            <th class="text-center v-col-6">
+              Value
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item, index) of paras">
+            <td class="text-center">{{item.name}}</td>
+            <td class="text-center">
+              <input type="number" class="text-center w-75" v-model="paras[index].value"/>
+            </td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+    <v-row class="my-4">
+      <v-col>
+        <div class="text-center text-h4 font-weight-bold">DH参数(SDH)</div>
+        <v-table>
+          <thead>
+          <tr>
+            <th class="text-center v-col-2">关节</th>
+            <th class="text-center v-col-2">theta</th>
+            <th class="text-center v-col-2">d</th>
+            <th class="text-center v-col-2">a</th>
+            <th class="text-center v-col-2">alpha</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td></td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+    <v-row class="my-4">
+      <v-col>
+        <div class="text-center text-h4 font-weight-bold">DH参数(MDH)</div>
+        <v-table>
+          <thead>
+          <tr>
+            <th class="text-center v-col-2">关节</th>
+            <th class="text-center v-col-2">alpha</th>
+            <th class="text-center v-col-2">a</th>
+            <th class="text-center v-col-2">d</th>
+            <th class="text-center v-col-2">theta</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td></td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
     <v-btn class="mx-auto d-block" @click="confirm">确认</v-btn>
   </v-card>
 </template>
