040a0fba1cdc9b404f137cab557e1503223a2612
Cybaster <1219925930@qq.com>
2023-06-15 22:17:11 +0800

    update default parameters

--- src/components/STLModel.vue
+++ src/components/STLModel.vue
@@ -124,7 +124,7 @@ const loadSTL = async () => {
     joints.push(joint)
     parent.add(joint)
 
-    const linkGeometry = await stlLoader.loadAsync("/" + type + "/meshes/link_" + (i + 1) + ".STL");
+    const linkGeometry = await stlLoader.loadAsync(process.env.BASE_URL + type + "/meshes/link_" + (i + 1) + ".STL");
     const linkMaterial = new THREE.MeshPhongMaterial({color:0xDDDADA})
     const link = new THREE.Mesh(linkGeometry, linkMaterial);
     link.position.set(...linkNodes[i + 1].getElementsByTagName("visual")[0].getElementsByTagName("origin")[0].getAttribute("xyz").split(" ").map(Number))
